a092e136145abad7e8e74ea8bf3b46cb1c3ff61a
bijoya <bijoya10101@gmail.com>
2020-12-28 00:16:10 +0530

    bs5

--- components/AppDrawerNavigator.js
+++ components/AppDrawerNavigator.js
@@ -2,11 +2,11 @@ import * as React from "react";
 import {createDrawerNavigator} from "react-navigation-drawer";
 import {AppTabNavigator} from "./AppTabNavigator";
 import CustomSideBarMenu from "./customSideBarMenu";
+import SettingsScreen from "../screens/SettingsScreen"
 
 export const AppDrawerNavigator=createDrawerNavigator({
-    Home:{
-        screen:AppTabNavigator
-        }
+    Home:{screen:AppTabNavigator},
+    Settings:{screen:SettingsScreen}
     },
     {
         contentComponent:CustomSideBarMenu
